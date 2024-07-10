bb1506a0aaa2f91a8a81f2b7e072c461449d328a
Tristan <talderson42@gmail.com>
2024-07-10 19:45:33 -0400

    added wifi connection ability to main.
    requires nvs_flash_init() to be called before wifi_init()

--- espGarden/main/main.cpp
+++ espGarden/main/main.cpp
@@ -2,6 +2,8 @@
 #include <inttypes.h>
 #include "esp_chip_info.h"
 #include "heltec_unofficial.h"
+#include "nvs_flash.h"
+#include "wifi_manager.h"
 
 // Define logging tag
 static const char* LOG_TAG = "HeltecGardenApp";
@@ -21,6 +23,13 @@ extern "C" void app_main() {
 
     // Setup Heltec device (OLED and LoRa)
     heltec_setup();
+    // needed for wifi
+    ESP_ERROR_CHECK(nvs_flash_init());
+
+    const char *ssid = "Test";
+    const char *pass = "123456789";
+    wifi(ssid, pass);
+    
 
     ssd1306_clear_screen(ssd1306_dev, 0x00);
     ssd1306_draw_string(ssd1306_dev, 0, 0, (const uint8_t*)"Initialization OK", 12, 1);
