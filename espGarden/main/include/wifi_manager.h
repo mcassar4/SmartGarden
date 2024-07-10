35c5f2a9b1075c8ac87251b0fff7d480d10980fc
Tristan <talderson42@gmail.com>
2024-07-10 19:44:04 -0400

    library for connecting to wifi using freeRTOS
     nvs_flash must called before initializing

---
+++ espGarden/main/include/wifi_manager.h
@@ -0,0 +1,109 @@
+#ifndef WIFI_MANAGER_H
+#define WIFI_MANAGER_H
+
+
+#include "wifi_manager.h"
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_err.h"
+#include "esp_netif.h"
+
+static const char *TAG = "wifi_manager";
+static EventGroupHandle_t wifi_event_group;
+static esp_netif_t *sta_netif = NULL;
+const int CONNECTED_BIT = BIT0;
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        esp_wifi_connect();
+        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+    }
+}
+
+esp_err_t wifi_manager_init(void) {
+    ESP_ERROR_CHECK(esp_netif_init());
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+
+    return ESP_OK;
+}
+
+esp_err_t wifi_manager_set_credentials(const char *ssid, const char *password) {
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = "",
+            .password = ""
+        },
+    };
+    strncpy((char *)wifi_config.sta.ssid, ssid, sizeof(wifi_config.sta.ssid));
+    strncpy((char *)wifi_config.sta.password, password, sizeof(wifi_config.sta.password));
+
+    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+
+    return ESP_OK;
+}
+
+// esp_err_t wifi_manager_set_certificates(const uint8_t *ca_cert, size_t ca_cert_len, const uint8_t *pac_file, size_t pac_file_len) {
+//     ESP_ERROR_CHECK(esp_eap_client_set_ca_cert(ca_cert, ca_cert_len));
+//     ESP_ERROR_CHECK(esp_eap_client_set_pac_file(pac_file, pac_file_len - 1));
+
+//     return ESP_OK;
+// }
+
+esp_err_t wifi_manager_connect(void) {
+    ESP_ERROR_CHECK(esp_wifi_start());
+    return ESP_OK;
+}
+
+void wifi_task(void *pvParameters) {
+    esp_netif_ip_info_t ip;
+    memset(&ip, 0, sizeof(esp_netif_ip_info_t));
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+    while (1) {
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+        if (esp_netif_get_ip_info(sta_netif, &ip) == 0) {
+            ESP_LOGI(TAG, "~~~~~~~~~~~");
+            ESP_LOGI(TAG, "IP:" IPSTR, IP2STR(&ip.ip));
+            ESP_LOGI(TAG, "MASK:" IPSTR, IP2STR(&ip.netmask));
+            ESP_LOGI(TAG, "GW:" IPSTR, IP2STR(&ip.gw));
+            ESP_LOGI(TAG, "~~~~~~~~~~~");
+        }
+    }
+}
+
+void wifi(const char *ssid, const char *password){
+    ESP_ERROR_CHECK(wifi_manager_init());
+
+    ESP_ERROR_CHECK(wifi_manager_set_credentials(ssid, password));
+
+    // If you have certificates, set them here
+    // ESP_ERROR_CHECK(wifi_manager_set_certificates(ca_pem_start, ca_pem_bytes, pac_file_pac_start, pac_file_bytes));
+
+    ESP_ERROR_CHECK(wifi_manager_connect());
+
+    // Create a task to monitor the connection status
+    xTaskCreate(&wifi_task, "wifi_task", 4096, NULL, 5, NULL);
+}
+#endif // WIFI_MANAGER_H
