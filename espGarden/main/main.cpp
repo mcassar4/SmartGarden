0e7f40be50fae4faab647f7f4fce11614cad17dd
Tristan Alderson <talderson@evertz.com>
2024-07-15 16:03:46 -0400

    Add NVS Flash Init Check

--- espGarden/main/main.cpp
+++ espGarden/main/main.cpp
@@ -24,7 +24,13 @@ extern "C" void app_main() {
 
     // Setup Heltec device (OLED and LoRa)
     heltec_setup();
-    // needed for wifi
+    /* Initialize NVS */
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
     const char *ssid = "Test";
     const char *pass = "123456789";
