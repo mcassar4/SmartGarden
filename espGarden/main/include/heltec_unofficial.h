9b85b4d1367d78055c4cc44efa24bfccde957226
mcassar4 <mcassar@evertz.com>
2024-07-18 11:02:57 -0400

    Refactor Heltec Dependancies and Drivers
    - Corrected pin definitions
    - Organized includes
    - Cleaned setup and loop
    - Fixed temperature sensor
    - Created simple display string function

--- espGarden/main/include/heltec_unofficial.h
+++ espGarden/main/include/heltec_unofficial.h
@@ -1,138 +1,186 @@
 #ifndef HELTEC_UNOFFICIAL_H
 #define HELTEC_UNOFFICIAL_H
 
+//################################################################################//
+
+// Define logging tags
+static const char* SETUP_LOG_TAG = "Setup";
+static const char* LOOP_LOG_TAG = "Loop";
+static const char* GARDEN_LOG_TAG = "Garden App";
+static const char* DISPLAY_LOG_TAG = "Display Manager";
+// static const char* DISPLAY_LOG_TAG = "WIFI Manager";
+
+//################################################################################//
+
+// Drivers
 #include "driver/gpio.h"
 #include "driver/adc.h"
 #include "driver/ledc.h"
-#include "driver/temp_sensor.h"
-#include "esp_sleep.h"
-#include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
+#include "driver/temperature_sensor.h"
 #include "driver/spi_master.h"
 #include "driver/i2c_master.h"
+
+// Esp Libraries
+#include "esp_sleep.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "esp_system.h"
+#include "esp_mac.h"
+
+// RTOS
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "sdkconfig.h"
 
+//NVS
+#include "nvs_flash.h"
+
+//ADC
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_continuous.h"
+
+
+// Heltec Dependencies
 #include "ssd1306.h"
 #include "RadioLib.h"
+#include "HotButton.h"
 
-#define USB_VID 0x303a
-#define USB_PID 0x1001
+// Custom Libraries
+// #include "wifi_manager.h"
 
+//################################################################################//
+
+// Stats
 #define EXTERNAL_NUM_INTERRUPTS 46
 #define NUM_DIGITAL_PINS        48
 #define NUM_ANALOG_INPUTS       20
 
-static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT+48;
-#define BUILTIN_LED  LED_BUILTIN // backward compatibility
-#define LED_BUILTIN LED_BUILTIN
-#define RGB_BUILTIN LED_BUILTIN
-#define RGB_BRIGHTNESS 64
+// USB
+#define USB_VID 0x303a
+#define USB_PID 0x1001
 
-#define analogInputToDigitalPin(p)  (((p)<20)?(analogChannelToDigitalPin(p)):-1)
-#define digitalPinToInterrupt(p)    (((p)<49)?(p):-1)
-#define digitalPinHasPWM(p)         (p < 46)
+// WIFI
+#define WIFI_SSID "Test"
+#define WIFI_PASS "123456789"
+
+// Little Orange LED
+#define LED_BUILTIN = SOC_GPIO_PIN_COUNT + 48;
+#define LED_PIN GPIO_NUM_35
+#define LED_FREQ 5000
+
+// Useful Pin Functions
+#define analogInputToDigitalPin(p)  (((p) < 20) ? (analogChannelToDigitalPin(p)) : -1)
+#define digitalPinToInterrupt(p)    (((p) < 49) ? (p) : -1)
+#define digitalPinHasPWM(p)         ((p) < 46)
 
 #define HIGH 1
 #define LOW 0
-#define BUTTON GPIO_NUM_0
 
 // Constants and GPIO definitions
-#define LED_PIN GPIO_NUM_35
-#define LED_FREQ 5000
-#define LED_CHAN 0
-#define LED_RES 8
+#define PRG_BUTTON GPIO_NUM_0
+HotButton button(PRG_BUTTON);
+#define USB_VID 0x303a
+#define USB_PID 0x1001
 
-#define VEXT GPIO_NUM_36
-#define VBAT_CTRL GPIO_NUM_37
-#define VBAT_ADC ADC1_CHANNEL_0
+#define VEXT_CTRL GPIO_NUM_36
+#define VBAT_READ GPIO_NUM_1
+#define ADC_CTRL GPIO_NUM_37
 
-#define SS GPIO_NUM_8
-#define SCK GPIO_NUM_9
-#define MOSI GPIO_NUM_10
-#define MISO GPIO_NUM_11
-
-#define DIO1 GPIO_NUM_14
-#define RST_LoRa GPIO_NUM_12
-#define BUSY_LoRa GPIO_NUM_13
-
+// Display
+#define DISPLAY_HEIGHT 64
+#define DISPLAY_WIDTH  128
 #define SDA_OLED GPIO_NUM_17
 #define SCL_OLED GPIO_NUM_18
 #define RST_OLED GPIO_NUM_21
+#define DIO0 GPIO_NUM_14;
+#define DIO1 DIO0;
 
-#define A0 = 1;
-#define A1 = 2;
-#define A2 = 3;
-#define A3 = 4;
-#define A4 = 5;
-#define A5 = 6;
-#define A6 = 7;
-#define A7 = 8;
-#define A8 = 9;
-#define A9 = 10;
-#define A10 = 11;
-#define A11 = 12;
-#define A12 = 13;
-#define A13 = 14;
-#define A14 = 15;
-#define A15 = 16;
-#define A16 = 17;
-#define A17 = 18;
-#define A18 = 19;
-#define A19 = 20;
-#define T1 = 1;
-#define T2 = 2;
-#define T3 = 3;
-#define T4 = 4;
-#define T5 = 5;
-#define T6 = 6;
-#define T7 = 7;
-#define T8 = 8;
-#define T9 = 9;
-#define T10 = 10;
-#define T11 = 11;
-#define T12 = 12;
-#define T13 = 13;
-#define T14 = 14;
+// LORA
+#define RST_LoRa = GPIO_NUM_12;
+#define BUSY_LoRa = GPIO_NUM_13;
 
+// Touch Pins
+#define TOUCH1 GPIO_NUM_1
+#define TOUCH2 GPIO_NUM_2
+#define TOUCH3 GPIO_NUM_3
+#define TOUCH4 GPIO_NUM_4
+#define TOUCH5 GPIO_NUM_5
+#define TOUCH6 GPIO_NUM_6
+#define TOUCH7 GPIO_NUM_7
 
-// static const uint8_t TX = 43;
-// static const uint8_t RX = 44;
+// ADC1 Channels
+#define ADC1_CH0 GPIO_NUM_1
+#define ADC1_CH1 GPIO_NUM_2
+#define ADC1_CH2 GPIO_NUM_3
+#define ADC1_CH3 GPIO_NUM_4
+#define ADC1_CH4 GPIO_NUM_5
+#define ADC1_CH5 GPIO_NUM_6
+#define ADC1_CH6 GPIO_NUM_7
 
-// Battery voltage scale constants
-const float min_voltage = 3.04;
-const float max_voltage = 4.26;
-const uint8_t scaled_voltage[100] = {
-    254, 242, 230, 227, 223, 219, 215, 213, 210, 207,
-    206, 202, 202, 200, 200, 199, 198, 198, 196, 196,
-    195, 195, 194, 192, 191, 188, 187, 185, 185, 185,
-    183, 182, 180, 179, 178, 175, 175, 174, 172, 171,
-    170, 169, 168, 166, 166, 165, 165, 164, 161, 161,
-    159, 158, 158, 157, 156, 155, 151, 148, 147, 145,
-    143, 142, 140, 140, 136, 132, 130, 130, 129, 126,
-    125, 124, 121, 120, 118, 116, 115, 114, 112, 112,
-    110, 110, 108, 106, 106, 104, 102, 101, 99, 97,
-    94, 90, 81, 80, 76, 73, 66, 52, 32, 7
-};
+// GPIO Pins
+#define GPIO1 GPIO_NUM_1
+#define GPIO2 GPIO_NUM_2
+#define GPIO3 GPIO_NUM_3
+#define GPIO4 GPIO_NUM_4
+#define GPIO5 GPIO_NUM_5
+#define GPIO6 GPIO_NUM_6
+#define GPIO7 GPIO_NUM_7
+#define GPIO19 GPIO_NUM_19
+#define GPIO20 GPIO_NUM_20
+#define GPIO21 GPIO_NUM_21
+#define GPIO26 GPIO_NUM_26
+#define GPIO33 GPIO_NUM_33
+#define GPIO34 GPIO_NUM_34
+#define GPIO35 GPIO_NUM_35
+#define GPIO36 GPIO_NUM_36
+#define GPIO37 GPIO_NUM_37
+#define GPIO38 GPIO_NUM_38
+#define GPIO39 GPIO_NUM_39
+#define GPIO40 GPIO_NUM_40
+#define GPIO41 GPIO_NUM_41
+#define GPIO42 GPIO_NUM_42
+#define GPIO43 GPIO_NUM_43
+#define GPIO44 GPIO_NUM_44
+#define GPIO45 GPIO_NUM_45
+#define GPIO46 GPIO_NUM_46
 
-// Function prototypes
+// UART Pins
+#define TX = GPIO_NUM_43;
+#define RX = GPIO_NUM_44;
+
+//################################################################################//
+
+extern ssd1306_handle_t display;
+
+//################################################################################//
+
 void heltec_setup();
 void heltec_loop();
 void heltec_deep_sleep(int seconds = 0);
 void heltec_led(int percent);
-void heltec_ve(bool state);
-float heltec_vbat();
-int heltec_battery_percent(float vbat = -1);
-bool heltec_wakeup_was_timer();
 float heltec_temperature();
-void heltec_display_power(bool on);
+bool heltec_wakeup_was_timer();
 
-// Global handle for the SSD1306
-extern ssd1306_handle_t ssd1306_dev;
-extern SX1276 radio; // Defined in main.cpp
+void init_nvs();
+void init_display();
+void display_centered_string(ssd1306_handle_t *display, const char *str, uint8_t font_size, uint32_t duration_ms);
 
-void i2c_master_init() {
+//################################################################################//
+
+void init_nvs(){
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+}
+
+void init_display() {
+    // Attach screen via I2C, log and return
+    ESP_LOGI(SETUP_LOG_TAG, "Initializing I2C...");
     i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
         .sda_io_num = SDA_OLED,
@@ -141,19 +189,11 @@ void i2c_master_init() {
         .scl_pullup_en = GPIO_PULLUP_ENABLE,
         .master = {
             .clk_speed = 400000
-        }
+        },
+        .clk_flags = 0
     };
     ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &conf));
     ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, conf.mode, 0, 0, 0));
-}
-
-
-void heltec_setup() {
-    static const char* LOG_TAG = "HeltecSetup";
-    esp_log_level_set(LOG_TAG, ESP_LOG_INFO);
-
-    ESP_LOGI(LOG_TAG, "Initializing I2C...");
-    i2c_master_init();
 
     // Toggle reset if connected
     gpio_set_direction(RST_OLED, GPIO_MODE_OUTPUT);
@@ -162,48 +202,85 @@ void heltec_setup() {
     gpio_set_level(RST_OLED, 1); // Out of reset
 
     // Initialize the display
-    ESP_LOGI(LOG_TAG, "Initializing SSD1306 display...");
-    ssd1306_dev = ssd1306_create(I2C_NUM_0, SSD1306_I2C_ADDRESS); // Check your I2C configuration
-    if (ssd1306_init(ssd1306_dev) == ESP_OK) {
-        ESP_LOGI(LOG_TAG, "Display initialized successfully");
+    ESP_LOGI(DISPLAY_LOG_TAG, "Initializing SSD1306 display...");
+    display = ssd1306_create(I2C_NUM_0, SSD1306_I2C_ADDRESS);
+    if (ssd1306_init(display) == ESP_OK) {
+        ESP_LOGI(DISPLAY_LOG_TAG, "Display initialized successfully");
+        ssd1306_clear_screen(display, 0x00);
+        ssd1306_refresh_gram(display);
     } else {
-        ESP_LOGE(LOG_TAG, "Failed to initialize display");
+        ESP_LOGE(DISPLAY_LOG_TAG, "Failed to initialize display");
     }
-
-    ssd1306_clear_screen(ssd1306_dev, 0x00);
-    ssd1306_draw_string(ssd1306_dev, 0, 0, (const uint8_t*)"Heltec ESP32 LoRa", 12, 1);
-    ssd1306_refresh_gram(ssd1306_dev);
-
-    // ADC configuration for battery monitoring
-    adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(VBAT_ADC, ADC_ATTEN_DB_11); // Adjust as necessary
-
-    ESP_LOGI(LOG_TAG, "Setup complete");
 }
 
-void heltec_loop() {
-    static const char* LOG_TAG = "HeltecLoop";
-    ESP_LOGI(LOG_TAG, "Loop running");
+//################################################################################//
 
-    // Handle button press for power management
-    int btn_level = gpio_get_level(BUTTON);
-    if (btn_level == 0) {  // Assuming active-low button
-        ESP_LOGI(LOG_TAG, "Button pressed, entering deep sleep");
-        heltec_deep_sleep(10); // Sleep for 10 seconds as an example
+// Helper Functions
+void display_centered_string(ssd1306_handle_t &display, const char *str, uint8_t font_size, uint32_t duration_ms) {
+    // Function to display a centered string for a specific duration
+    if (display == nullptr) {
+        ESP_LOGE(SETUP_LOG_TAG, "Display handle is NULL");
+        return;
     }
 
-    // Update display
-    ssd1306_clear_screen(ssd1306_dev, 0x00);
-    ssd1306_draw_string(ssd1306_dev, 0, 16, (const uint8_t*)"Running loop...", 12, 1);
-    ssd1306_refresh_gram(ssd1306_dev);
+    // Clear the screen
+    ssd1306_clear_screen(display, 0x00);
+    ssd1306_refresh_gram(display);
+
+    // Calculate string width and starting position to center the string
+    uint8_t str_width = font_size * strlen(str);
+    uint8_t start_x = (DISPLAY_WIDTH - str_width) / 2;
+    uint8_t start_y = (DISPLAY_HEIGHT - font_size) / 2;
+
+    // Draw the string at the calculated position
+    ssd1306_draw_string(display, start_x, start_y, (const uint8_t *)str, font_size, 1);
+
+    // Refresh the display to show the string
+    ssd1306_refresh_gram(display);
+
+    // Keep the display on for the specified duration, otherwise
+    if (duration_ms != 0){
+        vTaskDelay(pdMS_TO_TICKS(duration_ms));
+        // Clear the screen after the delay
+        ssd1306_clear_screen(display, 0x00);
+        ssd1306_refresh_gram(display);
+    }
+
+}
+
+//################################################################################//
+
+// Driver Functions
+void heltec_setup() {
+    esp_log_level_set(SETUP_LOG_TAG, ESP_LOG_INFO);
+    ESP_LOGI(SETUP_LOG_TAG, "Starting setup");
+
+    init_nvs();
+    init_display();
+
+    // init_wifi(WIFI_SSID, WIFI_PASS);
+    // ADC configuration for battery monitoring disabled for now
+    // adc1_config_width(ADC_WIDTH_BIT_12);
+    // adc1_config_channel_atten(VBAT_ADC, ADC_ATTEN_DB_11); // Adjust as necessary
+
+    display_centered_string(display, "ESP32 Setup Complete!", 12, 1000);
+    ESP_LOGI(SETUP_LOG_TAG, "Setup complete");
+}
+
+void heltec_loop(const int &delay) {
+    // ESP_LOGI(SETUP_LOG_TAG, "Loop running");
+
+    // Handle button press
+    int btn_level = gpio_get_level(PRG_BUTTON);
+    if (btn_level == 0) {  // Assuming active-low button
+        ESP_LOGI(LOOP_LOG_TAG, "Button pressed");
+        char temp_buffer[10];
+        sprintf(temp_buffer, "%f", heltec_temperature());
+        display_centered_string(display, temp_buffer, 12, 1000);
+    }
 
     // Delay in loop for readability in logs
-    vTaskDelay(pdMS_TO_TICKS(1000));
-}
-
-void heltec_deep_sleep(int seconds) {
-    esp_sleep_enable_timer_wakeup(seconds * 1000000);
-    esp_deep_sleep_start();
+    vTaskDelay(pdMS_TO_TICKS(delay));
 }
 
 void heltec_led(int percent) {
@@ -212,7 +289,8 @@ void heltec_led(int percent) {
         .duty_resolution = LEDC_TIMER_8_BIT,
         .timer_num = LEDC_TIMER_0,
         .freq_hz = LED_FREQ,
-        .clk_cfg = LEDC_AUTO_CLK
+        .clk_cfg = LEDC_AUTO_CLK,
+        .deconfigure = false
     };
     ledc_timer_config(&ledc_timer);
 
@@ -220,50 +298,57 @@ void heltec_led(int percent) {
         .gpio_num = LED_PIN,
         .speed_mode = LEDC_LOW_SPEED_MODE,
         .channel = LEDC_CHANNEL_0,
+        .intr_type = LEDC_INTR_DISABLE,
         .timer_sel = LEDC_TIMER_0,
-        .duty = (uint32_t)((255 * percent) / 100)
+        .duty = (uint32_t)((255 * percent) / 100),
+        .hpoint = 0,
+        .flags = {0},
     };
     ledc_channel_config(&ledc_channel);
 }
 
-void heltec_ve(bool state) {
-    gpio_set_direction(VEXT, GPIO_MODE_OUTPUT);
-    gpio_set_level(VEXT, state ? LOW : HIGH);
-}
-
-float heltec_vbat() {
-    gpio_set_direction(VBAT_CTRL, GPIO_MODE_OUTPUT);
-    gpio_set_level(VBAT_CTRL, LOW);
-    vTaskDelay(pdMS_TO_TICKS(10));
-    int raw = adc1_get_raw(VBAT_ADC);
-    gpio_set_direction(VBAT_CTRL, GPIO_MODE_INPUT);
-    return (raw / 4095.0) * 3.3;  // Assuming a max of 3.3V for ADC
-}
-
-int heltec_battery_percent(float vbat) {
-    if (vbat == -1) {
-        vbat = heltec_vbat();
-    }
-    int percentage = 0;
-    for (int i = 0; i < 100; i++) {
-        if (vbat >= min_voltage + (max_voltage - min_voltage) * (scaled_voltage[i] / 255.0)) {
-            percentage = 100 - i;
-            break;
-        }
-    }
-    return percentage;
-}
-
 float heltec_temperature() {
-    float temp;
-    temp_sensor_read_celsius(&temp);
+    esp_err_t ret;
+    float temp = 0.0;
+    temperature_sensor_handle_t temp_handle = NULL;
+    temperature_sensor_config_t temp_config = {
+        .range_min = -10,
+        .range_max = 80,
+    };
+
+    ret = temperature_sensor_install(&temp_config, &temp_handle);
+    if (ret != ESP_OK) {
+        ESP_LOGE(SETUP_LOG_TAG, "Failed to install temperature sensor");
+        return temp;
+    }
+
+    ret = temperature_sensor_enable(temp_handle);
+    if (ret != ESP_OK) {
+        ESP_LOGE(SETUP_LOG_TAG, "Failed to start temperature sensor");
+        temperature_sensor_uninstall(temp_handle);
+        return temp;
+    }
+
+    ret = temperature_sensor_get_celsius(temp_handle, &temp);
+    if (ret != ESP_OK) {
+        ESP_LOGE(SETUP_LOG_TAG, "Failed to read temperature");
+    }
+
+    temperature_sensor_disable(temp_handle);
+    temperature_sensor_uninstall(temp_handle);
     return temp;
 }
 
-bool heltec_wakeup_was_timer() {
-    return esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_TIMER;
-}
 
+// Deep Sleep disabled for now
+// void heltec_deep_sleep(int seconds) {
+//     esp_sleep_enable_timer_wakeup(seconds * 1000000);
+//     esp_deep_sleep_start();
+// }
+// bool heltec_wakeup_was_timer() {
+//     return esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_TIMER;
+// }
 
+//################################################################################//
 
 #endif // HELTEC_UNOFFICIAL_H
--- espGarden/main/main.cpp
+++ espGarden/main/main.cpp
@@ -1,34 +1,20 @@
+#include <algorithm>
 #include <stdio.h>
 #include <inttypes.h>
-#include "esp_chip_info.h"
+
 #include "heltec_unofficial.h"
 
-// Define logging tag
-static const char* LOG_TAG = "HeltecGardenApp";
+ssd1306_handle_t display;
 
-// LoRa configuration parameters
-#define FREQUENCY 915
-#define BANDWIDTH 250.0
-#define SPREADING_FACTOR 9
-#define TRANSMIT_POWER 0
-
-ssd1306_handle_t ssd1306_dev;
 
 extern "C" void app_main() {
-    // Initialize serial logging
-    esp_log_level_set(LOG_TAG, ESP_LOG_INFO);
-    ESP_LOGI(LOG_TAG, "Starting app_main");
-
-    // Setup Heltec device (OLED and LoRa)
     heltec_setup();
 
-    ssd1306_clear_screen(ssd1306_dev, 0x00);
-    ssd1306_draw_string(ssd1306_dev, 0, 0, (const uint8_t*)"Initialization OK", 12, 1);
-    ssd1306_refresh_gram(ssd1306_dev);
+    esp_log_level_set(GARDEN_LOG_TAG, ESP_LOG_INFO);
+    ESP_LOGI(GARDEN_LOG_TAG, "Starting Heltec Garden App");
 
+    const int loop_delay = 100; //ms
     while (true) {
-        // Simple delay to keep the task alive
-        vTaskDelay(pdMS_TO_TICKS(10000));
-        heltec_loop();
+        heltec_loop(loop_delay);
     }
 }
