e6366da57b6718bfa3fd4801d20fd4fdf0616606
arpankanwer <arpankanwer98@gmail.com>
2024-07-31 15:34:28 -0400

    #6 photos are now dynamically and randomly showed

--- Webpage/webpage/src/pages/gallery.jsx
+++ Webpage/webpage/src/pages/gallery.jsx
@@ -1,83 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import ImageGallery from "react-image-gallery";
 
-import '../App.css';
+import '../styles/style.css';
 import "react-image-gallery/styles/css/image-gallery.css";
 
 import NavBar from '../components/navBar';
 import Footer from '../components/footer';
 
+const importAll = (r) => {
+    return r.keys().map(r);
+};
+
 const Gallery = () => {
     const [images, setImages] = useState([]);
 
     useEffect(() => {
-
-        const imageList = [
-            {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            },
-            {
-                original: "https://i.pinimg.com/736x/13/04/7c/13047cc05340be6afe126ca5906591fe.jpg",
-                thumbnail: "https://i.pinimg.com/736x/13/04/7c/13047cc05340be6afe126ca5906591fe.jpg",
-            },
-            {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            },
-            {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            }, {
-                original: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-                thumbnail: "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-            },
-        ];
-
-        const shuffledImages = imageList.sort(() => Math.random() - 0.5);
-        // const shuffledImages = imageList;
+        const imageList = importAll(require.context('../../public/images/', false, /\.(png|jpg|svg|jfif)$/));
+        const formattedImages = imageList.map(image => ({
+            original: image,
+            thumbnail: image,
+        }));
+        const shuffledImages = formattedImages.sort(() => Math.random() - 0.5);
 
         setImages(shuffledImages);
     }, []);
@@ -86,7 +29,7 @@ const Gallery = () => {
         <div className="App">
             <NavBar />
             <main className="App-main">
-                <section>
+                <div className="gallery-container">
                     <ImageGallery items={images} showThumbnails={true}
                         showFullscreenButton={true}
                         showPlayButton={true}
@@ -99,8 +42,10 @@ const Gallery = () => {
                         showIndex={true}
                         showBullets={true}
                     />
-                </section>
+                </div>
             </main>
+            <div className='empty-height'></div>
+            <div className='empty-height'></div>
 
             <Footer />
         </div>
