e2572ebf7dad9db669c6be1682a745112b2d1554
mcassar4 <mcassar@evertz.com>
2024-08-13 15:04:55 -0400

    Add function to convert System Stare into Json format

--- espGarden/main/include/system_state.h
+++ espGarden/main/include/system_state.h
@@ -25,5 +25,68 @@ struct SystemState {
 static SystemState system_state;
 
 
+//################################################################################//
+// Json Construction
+
+std::string boolToString(bool value) {
+    return value ? "true" : "false";
+}
+
+std::string escapeJsonString(const std::string &input) {
+    std::ostringstream ss;
+    for (char c : input) {
+        switch (c) {
+            case '\"': ss << "\\\""; break;
+            case '\\': ss << "\\\\"; break;
+            case '\b': ss << "\\b"; break;
+            case '\f': ss << "\\f"; break;
+            case '\n': ss << "\\n"; break;
+            case '\r': ss << "\\r"; break;
+            case '\t': ss << "\\t"; break;
+            default: ss << c; break;
+        }
+    }
+    return ss.str();
+}
+
+// Function to convert the state to JSON
+std::string stateToJson() {
+    xSemaphoreTake(system_state.state_mutex, portMAX_DELAY);
+
+    std::ostringstream json;
+    json << "{";
+
+    json << "\"is_watering\":" << boolToString(system_state.is_watering) << ",";
+
+    json << "\"queue\":[";
+    std::queue<std::string> temp_queue = system_state.command_queue;
+    bool first = true;
+    while (!temp_queue.empty()) {
+        if (!first) {
+            json << ",";
+        }
+        json << "\"" << escapeJsonString(temp_queue.front()) << "\"";
+        temp_queue.pop();
+        first = false;
+    }
+    json << "],";
+
+    for (size_t i = 0; i < system_state.zones.size(); ++i) {
+        const WateringZone &zone = system_state.zones[i];
+        std::string zone_name = "Z" + std::to_string(i + 1);
+        json << "\"" << zone_name << "\":{";
+        json << "\"is_open\":" << boolToString(zone.is_open) << ",";
+        json << "\"timer\":" << zone.timer;
+        json << "}";
+        if (i != system_state.zones.size() - 1) {
+            json << ",";
+        }
+    }
+
+    json << "}";
+
+    xSemaphoreGive(system_state.state_mutex);
+    return json.str();
+}
 
 #endif // SYSTEM_STATE_H
