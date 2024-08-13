bd62a353b747a15a279bd7e22becbe4974d561f8
mcassar4 <mcassar@evertz.com>
2024-08-13 15:11:51 -0400

    Add watering command queue processing
    - String manipulation for incoming commands being sent to the queue
    - Z:M, where z can equal a zone number or "A" for all zones, and M is a number between 1 and 240 minutes

--- espGarden/main/include/system_state.h
+++ espGarden/main/include/system_state.h
@@ -25,6 +25,60 @@ struct SystemState {
 static SystemState system_state;
 
 
+bool add_watering_command_to_queue(std::string wateringCommand) {
+    // Command is verified by having a colon
+    size_t delimiter_pos = wateringCommand.find(':');
+    if (delimiter_pos != std::string::npos) {
+        std::string zone_str = wateringCommand.substr(0, delimiter_pos);
+        std::string time_str = wateringCommand.substr(delimiter_pos + 1);
+
+        try {
+            // Maximum number of hours for a zone to be open is 4
+            // Minimum number of minutes for a zone to be open is 1 minute
+            if (std::stoi(time_str) > 4*60 && std::stoi(time_str) < 1) {
+                ESP_LOGE(HTTP_LOG_TAG, "Watering time invalid! %s.", wateringCommand.c_str());
+                return false;
+            }
+        } catch(...) {
+            ESP_LOGE(HTTP_LOG_TAG, "Invalid watering time! %s.", wateringCommand.c_str());
+            return false;
+        }
+
+
+        std::string curr_zone_command;
+        if (zone_str == "A") {
+            unsigned curr_zone_num = 1;
+            for (WateringZone& zone: system_state.zones)
+            {
+                curr_zone_command = std::to_string(curr_zone_num) + ":" + time_str;
+                system_state.command_queue.push(curr_zone_command);
+                ESP_LOGI(HTTP_LOG_TAG, "Added command to queue: %s", curr_zone_command.c_str());
+                curr_zone_num++;
+            }
+            return true;
+        } else {
+            try {
+                if (std::stoi(zone_str) >= 1 && std::stoi(zone_str) <= system_state.zones.size()) {
+                    curr_zone_command = zone_str + ":" + time_str;
+                    system_state.command_queue.push(curr_zone_command);
+                    ESP_LOGI(HTTP_LOG_TAG, "Added command to queue: %s", curr_zone_command.c_str());
+                    return true;
+                } else {
+                    ESP_LOGE(HTTP_LOG_TAG, "Invalid zone number received %s.", wateringCommand.c_str());
+                    return false;
+                }
+            } catch (...) {
+                ESP_LOGE(HTTP_LOG_TAG, "Invalid command received %s.", wateringCommand.c_str());
+                return false;
+
+            }
+        }
+    }   
+
+    ESP_LOGE(HTTP_LOG_TAG, "Invalid Command Received %s.", wateringCommand.c_str());
+    return false;
+}
+
 //################################################################################//
 // Json Construction
 
