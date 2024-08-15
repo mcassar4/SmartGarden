f71cf628f9f9077b73260d6c517c63b9411e680a
mcassar4 <mcassar@evertz.com>
2024-08-15 10:08:21 -0400

    Refactor dependancies for home and footer

--- Webpage/webpage/src/components/footer.jsx
+++ Webpage/webpage/src/components/footer.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import '../styles/style.css';
 
 const Footer = () => {
--- Webpage/webpage/src/pages/home.jsx
+++ Webpage/webpage/src/pages/home.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import NavBar from "../components/navBar";
 import Footer from "../components/footer";
 import Weather from "./weather";
