dd6ca6321860a36280fcfdfe9b2d5b0072f62397
arpankanwer <arpankanwer98@gmail.com>
2024-08-28 10:29:46 -0400

    Displayed logo in footer

--- Webpage/webpage/src/components/footer.jsx
+++ Webpage/webpage/src/components/footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../styles/style.css';
+import logo from '../images/logo.png';
 
 const Footer = () => {
     return (
         <footer className="footer">
+            <img src={logo} alt="Logo" className="footer-logo" />
             <p>&copy; 2024 Evertz Garden. All rights reserved.</p>
         </footer>
     );
